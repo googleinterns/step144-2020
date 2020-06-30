0fdd24ec017f17fd3d37118289eae8b671bc759c
lzoebaker <lzoebaker@gmail.com>
2020-06-30 19:41:51 +0000

    Recommended career branch is found from quiz choices and outputted on new html page

--- src/main/java/com/google/sps/servlets/CareerQuizServlet.java
+++ src/main/java/com/google/sps/servlets/CareerQuizServlet.java
@@ -2,6 +2,8 @@ package com.google.sps.servlets;
 
 import com.google.sps.data.CareerQuestionDatabase;
 import com.google.sps.data.CareerQuestionAndChoices;
+import com.google.sps.data.CareerQuestionChoice;
+import com.google.sps.data.ProcessCareerQuizResults;
 import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,7 +19,10 @@ import com.google.inject.Singleton;
 /** Responds with a JSON string containing questions and answers for the career quiz*/
 public class CareerQuizServlet extends HttpServlet {
   private final CareerQuestionDatabase careerQuestionDatabase;
-  private static final String JSON_CONTENT_TYPE = "application/json;";
+  private ArrayList<CareerQuestionAndChoices> questionsAndChoices;
+  private static final String JSON_CONTENT_TYPE = "application/json";
+  private static final String QUIZ_SUBMIT = "career-quiz-submit";
+  private static final Gson gson = new Gson();
   
   @Inject public CareerQuizServlet(CareerQuestionDatabase careerQuestionDatabase) {
      this.careerQuestionDatabase = careerQuestionDatabase;
@@ -25,13 +30,35 @@ public class CareerQuizServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    ArrayList<CareerQuestionAndChoices> questionsAndChoices = 
-        this.careerQuestionDatabase.getQuestionsAndChoices();
+    this.questionsAndChoices = this.careerQuestionDatabase.getQuestionsAndChoices();
     // Convert the ArrayLists to JSON
     Gson gson = new Gson();
     String questionsAndChoicesJson = gson.toJson(questionsAndChoices);
-    // Send the JSON as the response
+    // Send the career questions and choices JSON as the response
     response.setContentType(JSON_CONTENT_TYPE);
     response.getWriter().println(questionsAndChoicesJson);
   }
+
+  @Override
+  /** when submit button on career quiz is clicked, this method is called and returns recommended path
+  based on selected choices*/
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    this.questionsAndChoices = this.careerQuestionDatabase.getQuestionsAndChoices();
+    String recommendedPath = handleQuizSubmission(request);
+    response.setContentType(JSON_CONTENT_TYPE);
+    String recommendedPathJson = gson.toJson(recommendedPath);
+    response.getWriter().println(recommendedPathJson);
+  }
+
+  private String handleQuizSubmission(HttpServletRequest request) {
+    ArrayList<CareerQuestionChoice> userChoices = new ArrayList();
+    for (CareerQuestionAndChoices questionAndChoice : this.questionsAndChoices) {
+      String selectedChoice = request.getParameter(questionAndChoice.getQuestion());
+      CareerQuestionChoice userChoice = gson.fromJson(selectedChoice, CareerQuestionChoice.class);
+      userChoices.add(userChoice);
+    }
+    String recommendedPath = ProcessCareerQuizResults.getRecommendedCareerPath(userChoices);
+    return recommendedPath;
+  }
+
 }
--- src/test/java/com/google/sps/CareerQuizTest.java
+++ src/test/java/com/google/sps/CareerQuizTest.java
@@ -24,12 +24,9 @@ import com.google.sps.data.CareerQuestionAndChoices;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
-import com.google.inject.testing.fieldbinder.Bind;
-import com.google.inject.testing.fieldbinder.BoundFieldModule;
-import com.google.inject.Inject;
-import com.google.inject.Guice;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.gson.Gson;
 
 
 /** Tests the CareerQuiz servlet and its interactions with CareerQuestionDatabase*/
@@ -40,31 +37,43 @@ public final class CareerQuizTest {
   @Mock private HttpServletRequest request;
   @Mock private HttpServletResponse response;
   private CareerQuestionDatabase careerQuestionDatabase;
-  
+  private CareerQuizServlet careerQuizServlet;
+  private static final Gson gson = new Gson();
+  private static final String CAREER_1 = "career1";
+  private static final String CAREER_2 = "career2";
+  private static final String CHOICE_1 = "choice1";
+  private static final String CHOICE_2 = "choice2";
   private static final List<CareerQuestionChoice> HARD_CODED_CHOICES = Arrays.asList(
-    new CareerQuestionChoice("choice1", "career1"),
-    new CareerQuestionChoice("choice2", "career2")
+    new CareerQuestionChoice(CHOICE_1, CAREER_1),
+    new CareerQuestionChoice(CHOICE_2, CAREER_2)
   );
   private static final String DATABASE_OBJECT_JSON = "[{\"question\":\"A question\",\"choices\":"
       + "[{\"choiceText\":\"choice1\",\"associatedCareerPath\":\"career1\"},"
       + "{\"choiceText\":\"choice2\",\"associatedCareerPath\":\"career2\"}]}]";
-
-  private void createInjector() {
-    Guice.createInjector(BoundFieldModule.of(this)).injectMembers(this);
-    MockitoAnnotations.initMocks(this);
-  }
+  private static final String QUESTION = "A question";
 
   @Before 
   public void setUp() {
     helper.setUp(); // initialize local datastore for testing
-    this.createInjector();
+    MockitoAnnotations.initMocks(this);
+    this.careerQuizServlet = this.createCareerQuestionServlet();
   }
 
   @After
   public void tearDown() {
     helper.tearDown();
   }
- 
+
+  private CareerQuizServlet createCareerQuestionServlet() {
+    DatastoreService localDatastore = DatastoreServiceFactory.getDatastoreService();
+    careerQuestionDatabase = new CareerQuestionDatabase(localDatastore); 
+    careerQuestionDatabase.putCareerQuestionAndChoicesIntoDatabase(
+        new CareerQuestionAndChoices(QUESTION, HARD_CODED_CHOICES));
+    CareerQuizServlet careerQuizServlet = new CareerQuizServlet(careerQuestionDatabase);
+    return careerQuizServlet;
+  }
+
+  /**Tests that the doGet method returns JSON containing database queried career question and choices*/
   @Test
   public void testCareerQuizServlet_OutputsJsonDatabaseQuestionAndChoices() throws IOException {
     // mocks the HttpServletResponse, which uses a writer to output JSON response
@@ -74,15 +83,31 @@ public final class CareerQuizTest {
     when(this.response.getWriter()).thenReturn(printWriter);
     
     // mocks the result of querying the CareerQuestionDatabase for all the Career Question and choices
-    DatastoreService localDatastore = DatastoreServiceFactory.getDatastoreService();
-    careerQuestionDatabase = new CareerQuestionDatabase(localDatastore); 
-    careerQuestionDatabase.putCareerQuestionAndChoicesIntoDatabase(
-        new CareerQuestionAndChoices("A question", HARD_CODED_CHOICES));
-    CareerQuizServlet careerQuizServlet = new CareerQuizServlet(careerQuestionDatabase);
-    careerQuizServlet.doGet(this.request, this.response);
+    
+    this.careerQuizServlet.doGet(this.request, this.response);
+
     // checks that the string writer used in servlet mock response contains the database object JSON
     // that matches with the hardcoded CareerQAndChoice given be the mock database
-    Assert.assertTrue(stringWriter.toString().contains(DATABASE_OBJECT_JSON));
+    String expected = DATABASE_OBJECT_JSON;
+    String result = stringWriter.toString();
+    Assert.assertTrue(result.contains(expected));
   }
+  
+  /** Tests that the doPost methods responds with the recommended String career path */
+  @Test 
+  public void testPostMethodRespondsWith_RecommendedCareerPath() throws IOException {
+    // mocks user behavior of selecting the choice 2 radio button on the career quiz
+    String choice2_json = gson.toJson(new CareerQuestionChoice(CHOICE_2, CAREER_2));
+    when(this.request.getParameter(QUESTION)).thenReturn(choice2_json);
 
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(this.response.getWriter()).thenReturn(writer);
+    
+    this.careerQuizServlet.doPost(this.request, this.response);
+
+    String expected = CAREER_2;
+    String result = stringWriter.toString();
+    Assert.assertTrue(result.contains(expected));
+  }
 }
