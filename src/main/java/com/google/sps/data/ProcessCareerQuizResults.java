602b366b9fe288b21f78b198e895a97c9b6cb4b5
Lauren Zoe Baker <lzoebaker@gmail.com>
2020-06-30 13:11:20 -0600

    Process career quiz results by returning most career path most frequently associated (#42)

---
+++ src/main/java/com/google/sps/data/ProcessCareerQuizResults.java
@@ -0,0 +1,40 @@
+package com.google.sps.data;
+
+import java.util.List;
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Map.Entry;
+import com.google.sps.data.CareerQuestionChoice;
+
+/** Returns the recommended career path based off the career quiz results. */
+public class ProcessCareerQuizResults {
+  private ProcessCareerQuizResults() {};
+  
+  public static String getRecommendedCareerPath(List<CareerQuestionChoice> userChoices) {
+    Map<String, Integer> choiceFrequencyHashMap = new HashMap<String, Integer>(); 
+    String careerPath;
+    String maxFrequencyCareerPath = new String();
+    Integer count;
+    // creates a frequency choiceFrequencyMap mapping associated career paths of choices to their frequencies
+    for (CareerQuestionChoice choice : userChoices) {
+      careerPath = choice.getAssociatedCareerPath();
+      count = choiceFrequencyHashMap.getOrDefault(careerPath, 0);
+      choiceFrequencyHashMap.put(careerPath, count + 1);
+    }
+    return getMostFrequentChoice(choiceFrequencyHashMap);
+  }
+
+  private static String getMostFrequentChoice(Map<String, Integer> choiceFrequencyMap) {
+    int maxFrequencyCount = 0;
+    int currentFrequency;
+    String mostFrequentKey = "";
+    for (String key : choiceFrequencyMap.keySet()) {
+      currentFrequency = choiceFrequencyMap.get(key);
+      if (currentFrequency > maxFrequencyCount) {
+        mostFrequentKey = key;
+        maxFrequencyCount = currentFrequency;
+      }
+    }
+    return mostFrequentKey;
+  }
+}
