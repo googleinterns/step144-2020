2c248a5f9f3ad68de1589e231b34c64260f3d178
leahatt <lattai>
2020-07-06 16:48:51 +0000

    Making changes from leah-dev to leav-dev2

--- src/main/java/com/google/sps/data/Player.java
+++ src/main/java/com/google/sps/data/Player.java
@@ -1,51 +1,21 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
 package com.google.sps.data;
 
-<<<<<<< HEAD
-import com.google.appengine.api.datastore.Key;
-
-// Player object to represent each user.
- public final class Player {
-    private String displayName;
-    private String email;
-    private String id;
-    private String imageID;
-    private String currentPageID;
-=======
 public class Player {
   private String displayName;
   private String email;
   private String id;
   private String imageID;
   private String currentPageID;
->>>>>>> 6365ee6688eb4911b2980634fa7ec96fc681536d
 
   public Player(String displayName, String email) {
     this.displayName = displayName;
     this.email = email;
   }
 
-<<<<<<< HEAD
-    public String getDisplayName() {
-        return displayName;
-    }
-=======
   public String getdisplayName() {
     return displayName;
   }
->>>>>>> 6365ee6688eb4911b2980634fa7ec96fc681536d
 
   public String getEmail() {
     return email;
