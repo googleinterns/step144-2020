ad88877e38534d0a64910b3d0a8f261813e760f7
Lauren Zoe Baker <lzoebaker@gmail.com>
2020-07-10 15:09:44 -0600

    Creating a servlet to handle inputting in static career quiz servlet data (#93)
     Creating a servlet to handle inputting in static career quiz servlet data

---
+++ src/main/java/com/google/sps/servlets/AddToDatabaseServlet.java
@@ -0,0 +1,80 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.QuestionChoice;
+import com.google.sps.data.QuestionDatabase;
+import com.google.sps.data.QuizQuestion;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.PatternSyntaxException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/addToDatabase")
+/** Responds with a JSON string containing questions and answers for the career quiz */
+public class AddToDatabaseServlet extends HttpServlet {
+  private static final String JSON_CONTENT_TYPE = "application/json";
+  private static final String CAREER_QUIZ_FORM_SUBMIT_PARAMETER = "careerQuizSubmit";
+  private static final String CAREER_QUIZ_QUESTION_PARAMETER = "careerQuestion";
+  private static final String CAREER_QUIZ_CHOICETEXT_PARAMETER = "careerChoiceText";
+  private static final String CAREER_QUIZ_CAREERPATH_PARAMETER = "careerPath";
+  private static final String CAREER_DATABASE_ENTITY_QUERY_STRING = "careerQuizQuestionAndChoices";
+  private static final String CANT_SPLIT_STRING =
+      "Choices and career paths need to be split by semicolons.";
+  private static final String NUMBER_CHOICES_DONT_MATCH_CAREER_PATHS =
+      "The number of choices given is not the same as the number of career paths given.";
+  private static final Gson gson = new Gson();
+  private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  private ArrayList<QuizQuestion> quizQuestions;
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    if (request.getParameter(CAREER_QUIZ_FORM_SUBMIT_PARAMETER) != null) {
+      addCareerQuizQuestion(request, response);
+    }
+  }
+
+  private void addCareerQuizQuestion(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    QuestionDatabase careerQuestionDatabase =
+        new QuestionDatabase(datastore, CAREER_DATABASE_ENTITY_QUERY_STRING);
+    boolean validInput = true;
+    String question = new String();
+    List<String> choiceText = new ArrayList();
+    List<String> careerPaths = new ArrayList();
+    try {
+      question = request.getParameter(CAREER_QUIZ_QUESTION_PARAMETER);
+      choiceText = Arrays.asList(request.getParameter(CAREER_QUIZ_CHOICETEXT_PARAMETER).split(";"));
+      careerPaths =
+          Arrays.asList(request.getParameter(CAREER_QUIZ_CAREERPATH_PARAMETER).split(";"));
+    } catch (PatternSyntaxException e) {
+      validInput = false;
+      handleInvalidInputError(CANT_SPLIT_STRING, response);
+    }
+    if (choiceText.size() != careerPaths.size()) {
+      validInput = false;
+      handleInvalidInputError(NUMBER_CHOICES_DONT_MATCH_CAREER_PATHS, response);
+    }
+    if (validInput) {
+      ArrayList<QuestionChoice> choices = new ArrayList();
+      for (int i = 0; i < choiceText.size(); i++) {
+        QuestionChoice newChoice = new QuestionChoice(choiceText.get(i), careerPaths.get(i));
+        choices.add(newChoice);
+      }
+      careerQuestionDatabase.putQuizQuestionsIntoDatabase(new QuizQuestion(question, choices));
+    }
+  }
+
+  private void handleInvalidInputError(String message, HttpServletResponse response)
+      throws IOException {
+    // this message will be turned into an alert box
+    response.setContentType(JSON_CONTENT_TYPE);
+    response.getWriter().println(gson.toJson(message));
+  }
+}
---
+++ src/test/java/com/google/sps/AddToDatabaseTest.java
@@ -0,0 +1,146 @@
+package com.google.sps;
+
+import static org.mockito.Mockito.when;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.sps.data.QuestionChoice;
+import com.google.sps.data.QuestionDatabase;
+import com.google.sps.data.QuizQuestion;
+import com.google.sps.servlets.AddToDatabaseServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/** Tests that the AddToDatabaseServlet correctly adds static entities to the game datastore */
+@RunWith(JUnit4.class)
+public final class AddToDatabaseTest {
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+  @Mock private HttpServletRequest request;
+  @Mock private HttpServletResponse response;
+  private QuestionDatabase questionDatabase;
+  private AddToDatabaseServlet addToDatabaseServlet;
+  private static final Gson gson = new Gson();
+  private static final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  private static final String QUESTION_1 = "A question";
+  private static final String CAREER_1 = "career1";
+  private static final String CAREER_2 = "career2";
+  private static final String CAREER_3 = "career3";
+  private static final String CHOICE_1 = "choice1";
+  private static final String CHOICE_2 = "choice2";
+  private static final String CHOICE_3 = "choice3";
+  private static final String CAREER_QUIZ_FORM_SUBMIT_PARAMETER = "careerQuizSubmit";
+  private static final String CAREER_QUIZ_QUESTION_PARAMETER = "careerQuestion";
+  private static final String CAREER_QUIZ_CHOICETEXT_PARAMETER = "careerChoiceText";
+  private static final String CAREER_QUIZ_CAREERPATH_PARAMETER = "careerPath";
+  private static final String CAREER_DATABASE_ENTITY_QUERY_STRING = "careerQuizQuestionAndChoices";
+  private static final String NUMBER_CHOICES_DONT_MATCH_CAREER_PATHS =
+      "The number of choices given is not the same as the number of career paths given.";
+  private static final List<QuestionChoice> QUESTION_CHOICES =
+      Arrays.asList(
+          new QuestionChoice(CHOICE_1, CAREER_1),
+          new QuestionChoice(CHOICE_2, CAREER_2),
+          new QuestionChoice(CHOICE_3, CAREER_3));
+
+  @Before
+  public void setUp() {
+    helper.setUp(); // initialize local datastore for testing
+    MockitoAnnotations.initMocks(this);
+    this.addToDatabaseServlet = new AddToDatabaseServlet();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  /**
+   * Tests that the doPost method fetches form parameters, creates appropriate career quiz
+   * questions, and adds to database
+   */
+  @Test
+  public void testValidInputIsAddedToDatabase() throws IOException {
+    // mocks the HttpServletResponse, which uses a writer to output JSON response
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter printWriter = new PrintWriter(stringWriter);
+
+    String questionInput = QUESTION_1;
+    String choiceTextInput = CHOICE_1 + ";" + CHOICE_2 + ";" + CHOICE_3;
+    String careerPathInput = CAREER_1 + ";" + CAREER_2 + ";" + CAREER_3;
+
+    when(this.response.getWriter()).thenReturn(printWriter);
+    mockUserCareerQuestionInput(questionInput, choiceTextInput, careerPathInput);
+
+    this.addToDatabaseServlet.doPost(this.request, this.response);
+
+    QuestionDatabase questionDatabase =
+        new QuestionDatabase(datastore, CAREER_DATABASE_ENTITY_QUERY_STRING);
+    ArrayList<QuizQuestion> databaseQuestions = questionDatabase.getQuizQuestions();
+    Assert.assertEquals(databaseQuestions.size(), 1);
+
+    // converting to JsonElements to do deep equality checks
+    JsonElement result = JsonParser.parseString(gson.toJson(databaseQuestions.get(0)));
+    JsonElement expected =
+        JsonParser.parseString(gson.toJson(new QuizQuestion(QUESTION_1, QUESTION_CHOICES)));
+    Assert.assertEquals(result, expected);
+  }
+
+  /**
+   * Tests that the doPost method fetches form parameters, creates appropriate career quiz
+   * questions, and adds to database
+   */
+  @Test
+  public void testInvalidInputTriggersErrorMessage_AndNothingAddedToDatabase() throws IOException {
+    // mocks the HttpServletResponse, which uses a writer to output JSON response
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter printWriter = new PrintWriter(stringWriter);
+
+    String questionInput = QUESTION_1;
+    String choiceTextInput = CHOICE_1 + ";" + CHOICE_3;
+    String careerPathInput = CAREER_1 + ";" + CAREER_2 + ";" + CAREER_3;
+
+    when(this.response.getWriter()).thenReturn(printWriter);
+    mockUserCareerQuestionInput(questionInput, choiceTextInput, careerPathInput);
+
+    this.addToDatabaseServlet.doPost(this.request, this.response);
+
+    QuestionDatabase questionDatabase =
+        new QuestionDatabase(datastore, CAREER_DATABASE_ENTITY_QUERY_STRING);
+    ArrayList<QuizQuestion> databaseQuestions = questionDatabase.getQuizQuestions();
+    // check that nothing has been added to the database
+    Assert.assertEquals(databaseQuestions.size(), 0);
+
+    JsonElement result = JsonParser.parseString(stringWriter.toString());
+    JsonElement expected =
+        JsonParser.parseString(gson.toJson(NUMBER_CHOICES_DONT_MATCH_CAREER_PATHS));
+    Assert.assertEquals(result, expected);
+  }
+
+  private void mockUserCareerQuestionInput(
+      String questionInput, String choiceTextInput, String careerPathInput) {
+    when(this.request.getParameter(CAREER_QUIZ_FORM_SUBMIT_PARAMETER))
+        .thenReturn(CAREER_QUIZ_FORM_SUBMIT_PARAMETER);
+    when(this.request.getParameter(CAREER_QUIZ_QUESTION_PARAMETER)).thenReturn(questionInput);
+    when(this.request.getParameter(CAREER_QUIZ_CHOICETEXT_PARAMETER)).thenReturn(choiceTextInput);
+    when(this.request.getParameter(CAREER_QUIZ_CAREERPATH_PARAMETER)).thenReturn(careerPathInput);
+  }
+}
