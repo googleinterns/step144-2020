c44670b59e563f07cafca4b0c90c9aa344b7c315
leahatt <lattai>
2020-07-17 21:12:47 +0000

    Integrated the character design functionality with the rest of the site and linked the pages together.

--- src/main/java/com/google/sps/servlets/ImageHandlerServlet.java
+++ src/main/java/com/google/sps/servlets/ImageHandlerServlet.java
@@ -41,7 +41,7 @@ public class ImageHandlerServlet extends HttpServlet {
   private static final String IMAGE_PARAMETER = "image";
   private static final String DEFAULT_PARAMETER = "default";
   private static final String PLAYER_QUERY_PARAMETER = "Player";
-  private static final String UPLOADED_REDIRECT_PARAMETER = "/uploaded.html";
+  private static final String UPLOADED_REDIRECT_PARAMETER = "/gameStage.html";
   private static final String LOGIN_REDIRECT_PARAMETER = "/userAuthPage.html";
   private static final String CHARACTER_DESIGN_REDIRECT_PARAMETER = "/characterDesign.html";
   private static final String CONTENT_TYPE = "text/html";
