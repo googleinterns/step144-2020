63dede133b3ff0398b05f59ad2e9a6da85dbae0d
leahatt <lattai>
2020-07-24 17:21:56 +0000

    Show exp in frontend and have it dynamically update the datastore.

--- src/main/webapp/gameDialogue.js
+++ src/main/webapp/gameDialogue.js
@@ -15,11 +15,16 @@ const FINAL_STAGE_BUTTON_VALUE = 'COMPLETE PATH';
 const FINAL_STAGE_REDIRECTION = 'CompletedPath.html';
 
 var dialogueArray;
-var i;
+var dialogueRegex;
+var exp;
+var threshold = 15;
+
 function loadFunctions() {
   modifyIfFinalStage();
+  workPromoButtonSwitch();
   getImage();
   getDialogue();
+  getExp();
 }
 
 function getDialogue() {
@@ -35,16 +40,16 @@ function handleResponse(response) {
 function addDialogueToDom(dialogue) {
   const quoteContainer = document.getElementById('dialogue-container');
   dialogueArray = dialogue.split(";");
-  i = 0;
-  quoteContainer.innerText = dialogueArray[i];
+  dialogueRegex = 0;
+  quoteContainer.innerText = dialogueArray[dialogueRegex];
 }
 
 function nextLine() {
   const quoteContainer = document.getElementById('dialogue-container');
-  if (i < dialogueArray.length-1){
-    i ++;
+  if (dialogueRegex < dialogueArray.length-1){
+    dialogueRegex ++;
   }
-  quoteContainer.innerText = dialogueArray[i];
+  quoteContainer.innerText = dialogueArray[dialogueRegex];
 }
 
 
@@ -91,4 +96,62 @@ function modifyIfFinalStage() {
           }
         }
       });
+}
+
+function getExp() {
+  fetch("/experience")
+  .then(response => response.text())
+  .then(expString => {
+    exp = parseInt(expString);
+    console.log("EXPERIENCE: " + exp);
+    showExp(exp);
+  });
+}
+
+function showExp(exp) {
+  const expContainer = document.getElementById('exp-container');
+  expContainer.innerText = "EXP:" + exp;
+}
+
+function changeExp() {
+  exp ++;
+  workPromoButtonSwitch();
+  showExp(exp);
+  const params = new URLSearchParams();
+  params.append('experience', exp);
+  fetch('/experience', {method: 'POST', body: params});
+}
+
+function workPromoButtonSwitch() {
+  if (exp == threshold) {
+    // Changes button to "TRY FOR PROMOTION"
+    const promoButton = document.getElementById('promotion-button');
+    promoButton.innerText = "TRY FOR PROMOTION";
+  } else if (exp == threshold + 1) {
+    // Changes button link to promotion.html
+    showPromoButton(true, null);
+  }
+  else {
+    // Earning experience points
+    showPromoButton(false, changeExp);
+  }
+}
+
+function showPromoButton(isLinkActive, onclick) {
+  const promoButton = document.getElementById('promotion-button');
+  const promoLink = document.getElementById('promotion-link');
+  if (isLinkActive) {
+    promoLink.href = "promotionquiz.html";
+  } else {
+    promoLink.removeAttribute("href");
+  }
+  promoButton.onclick = onclick;
+  promoButton.innerText = getWorkButtonTask();
+}
+
+function getWorkButtonTask() {
+  var taskArray = ["WRITE SOME CODE", "ASK MANAGER FOR FEEDBACK",
+      "ATTEND A MEETING", "TEST YOUR CODE", "FIX A BUG", "GET SOME COFFEE", "DEBUG A CODE"];
+  var randomNum = Math.floor(Math.random() * taskArray.length);
+  return taskArray[randomNum];
 }
